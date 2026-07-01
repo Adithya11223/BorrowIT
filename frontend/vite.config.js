aee9982f956b14de15f2457d7264aa2d282750d4
Adithya11223 <adithya70755@gmail.com>
2026-07-01 17:14:46 +0530

    Fix CORS asset load issue by removing crossorigin attribute in build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const removeCrossorigin = () => {
+  return {
+    name: 'remove-crossorigin',
+    transformIndexHtml(html) {
+      return html.replace(/\s+crossorigin(="anonymous"|="use-credentials"|"")?/gi, '');
+    },
+  };
+};
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), removeCrossorigin()],
   server: {
     port: 3000,
     host: true
